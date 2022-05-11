f2ddb3f24c85ae68cf0c80eb3475dd49344d1dd0
CalebHong <sanggigom@gmail.com>
2022-05-11 16:48:47 +0900

    22.05.11 업데이트

---
+++ Input/InputManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace SK
+{
+    public class InputManager : MonoBehaviour
+    {
+        public PlayerInput playerInput;
+
+        
+    }
+}
